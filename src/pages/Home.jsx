63ff0ee168fdee12465aaa064f37858c5c45d9cb
santos-eric <esantosdev77@gmail.com>
2023-11-21 05:56:13 -0700

    added skull to home page and defined properties

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+
+import Skull from '../models/Skull'
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        PopUp
+      </div> */}
 
 const Home = () => {
+
+  // skull default properties for mobile
+  const adjustSkullForScreenSize = () => {
+    let screenScale = null
+    let screenPosition = [0, -6.5, -43]
+    let rotation = [0.1, 4.7, 0]
+
+    if (window.innerWidth < 768) {
+      // [x,y,z] scale
+      screenScale = [0.9, 0.9, 0.9]
+    } else {
+      screenScale = [1, 1, 1]
+    }
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [skullScale, skullPosition, skullRotation] = adjustSkullForScreenSize()
+
   return (
-    <div>Home</div>
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{near:0.1, far:1000}}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Skull
+          position={skullPosition}
+            scale={skullScale}
+            rotation={skullRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
